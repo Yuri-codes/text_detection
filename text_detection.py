2be37655cc7867eda9ca41aef5ce3e03b73139ed
Yuri-codes <52564127+Yuri-codes@users.noreply.github.com>
2019-12-29 18:59:06 +0300

    Update text_detection.py

--- text_detection.py
+++ text_detection.py
@@ -1,35 +1,17 @@
 # USAGE
 # python text_detection.py --image images/lebron_james.jpg --east frozen_east_text_detection.pb
 
-# import the necessary packages
-#from imutils.object_detection import non_max_suppression
-#import numpy as np
-import argparse
 import time
 import cv2
 
-# construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", type=str,
-	help="path to input image")
-ap.add_argument("-east", "--east", type=str,
-	help="path to input EAST text detector")
-ap.add_argument("-c", "--min-confidence", type=float, default=0.5,
-	help="minimum probability required to inspect a region")
-ap.add_argument("-w", "--width", type=int, default=320,
-	help="resized image width (should be multiple of 32)")
-ap.add_argument("-e", "--height", type=int, default=320,
-	help="resized image height (should be multiple of 32)")
-args = vars(ap.parse_args())
-
 # load the input image and grab the image dimensions
-image = cv2.imread(args["image"])
+image = cv2.imread('images/photo1.jpg')
 orig = image.copy()
 (H, W) = image.shape[:2]
 
 # set the new width and height and then determine the ratio in change
 # for both the width and height
-(newW, newH) = (args["width"], args["height"])
+(newW, newH) = (320, 320)
 rW = W / float(newW)
 rH = H / float(newH)
 
@@ -46,7 +28,7 @@ layerNames = [
 
 # load the pre-trained EAST text detector
 print("[INFO] loading EAST text detector...")
-net = cv2.dnn.readNet(args["east"])
+net = cv2.dnn.readNet('frozen_east_text_detection.pb')
 
 # construct a blob from the image and then perform a forward pass of
 # the model to obtain the two output layer sets
@@ -64,7 +46,6 @@ print("[INFO] text detection took {:.6f} seconds".format(end - start))
 # initialize our set of bounding box rectangles and corresponding
 # confidence scores
 (numRows, numCols) = scores.shape[2:4]
-#rects = []
 confidences = []
 
 # loop over the number of rows
@@ -82,54 +63,9 @@ for y in range(0, numRows):
 	# loop over the number of columns
 	for x in range(0, numCols):
 		# if our score does not have sufficient probability, ignore it
-		if scoresData[x] < args["min_confidence"]:
+		if scoresData[x] < 0.5:
 			continue
-
-		# compute the offset factor as our resulting feature maps will
-		# be 4x smaller than the input image
-		#(offsetX, offsetY) = (x * 4.0, y * 4.0)
-
-		# extract the rotation angle for the prediction and then
-		# compute the sin and cosine
-		#angle = anglesData[x]
-		#cos = np.cos(angle)
-		#sin = np.sin(angle)
-
-		# use the geometry volume to derive the width and height of
-		# the bounding box
-		#h = xData0[x] + xData2[x]
-		#w = xData1[x] + xData3[x]
-
-		# compute both the starting and ending (x, y)-coordinates for
-		# the text prediction bounding box
-		#endX = int(offsetX + (cos * xData1[x]) + (sin * xData2[x]))
-		#endY = int(offsetY - (sin * xData1[x]) + (cos * xData2[x]))
-		#startX = int(endX - w)
-		#startY = int(endY - h)
-
-		# add the bounding box coordinates and probability score to
-		# our respective lists
-		#rects.append((startX, startY, endX, endY))
 		confidences.append(scoresData[x])
-
-# apply non-maxima suppression to suppress weak, overlapping bounding
-# boxes
-#boxes = non_max_suppression(np.array(rects), probs=confidences)
-
-# loop over the bounding boxes
-#for (startX, startY, endX, endY) in boxes:
-	# scale the bounding box coordinates based on the respective
-	# ratios
-	#startX = int(startX * rW)
-	#startY = int(startY * rH)
-	##endY = int(endY * rH)
-
-	# draw the bounding box on the image
-	#cv2.rectangle(orig, (startX, startY), (endX, endY), (0, 255, 0), 2)
-
-# show the output image
-#cv2.imshow("Text Detection", orig)
-#cv2.waitKey(0)
 
 if confidences == []:
 	print('good')
